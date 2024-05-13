219714acd46d59f149b1db270cf521e36a73b742
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2024-05-13 15:36:23 +1000

    basic test commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test"
+
+test.only("Basic test", async ({ page }) => {
+    await page.goto("https://demoblaze.com/")
+    await page.locator("#login2").click()
+    await page.locator("#loginusername").fill("test")
+    await page.locator("#loginpassword").fill("test")
+    await page.locator('button[onclick="logIn()"]').click()
+    await expect(page.locator('#logout2')).toBeVisible()
+
+})
