d69617dcde8467a0cef7083881b589eb14df6809
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2024-05-15 12:19:02 +1000

    set headless to true

--- global-setup.ts
+++ global-setup.ts
@@ -1,7 +1,7 @@
 import { expect, Browser, chromium, Page } from "@playwright/test";
 
 async function globalSetup() {
-    const browser: Browser = await chromium.launch({ headless: false })
+    const browser: Browser = await chromium.launch({ headless: true })
     const context = await browser.newContext()
     const page: Page = await context.newPage()
     await page.goto("https://demoblaze.com/")
